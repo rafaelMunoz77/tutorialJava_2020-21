809cb1f69ed332c16fafafc3333c30bee02e02dc
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-09-23 15:22:38 +0200

    Corrección de Hola Mundo

--- src/tutorialJava/capitulo1/Ejemplo01_HolaMundo.java
+++ src/tutorialJava/capitulo1/Ejemplo01_HolaMundo.java
@@ -3,13 +3,7 @@ package tutorialJava.capitulo1;
 public class Ejemplo01_HolaMundo {
 
 	public static void main(String[] args) {
-		int miNumero;
-		miNumero = 10;
-		int otroNumero = 15;
-		int suma;
-		suma = miNumero + otroNumero;
-		System.out.println(suma);
-		double miNumeroReal = 14.5;
+		System.out.println("Hola Mundo");
 	}     
 
 }
